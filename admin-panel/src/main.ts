0ddd704263831c4867b46a67e7535583294356ac
Talented-Dev934 <alexbenny544@gmail.com>
2024-10-24 23:04:26 +0300

    Update main.ts

--- admin-panel/src/main.ts
+++ admin-panel/src/main.ts
@@ -3,5 +3,6 @@ import 'hammerjs';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
